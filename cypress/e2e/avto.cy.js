d57da6322c3791e56984d00fbb8213c6553f7430
Olga <132088798+OlgaTsymbalova-QA@users.noreply.github.com>
2023-05-28 19:20:41 +0300

    Add files via upload

---
+++ cypress/e2e/avto.cy.js
@@ -0,0 +1,54 @@
+
+describe('Авторизация', function () {
+    
+    it('Верный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon')
+     })
+     it('Восстановление пароля', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.contains('Успешно отправили пароль на e-mail');
+        cy.get('#exitMessageButton > .exitIcon')
+     })
+     it('Верный логин, неверный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('ihello123Q');
+        cy.get('#loginButton').click();
+        cy.contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon')
+     })
+     it('Неверный логин, верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('ge@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon')
+     })
+     it('Негативная валидация', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Нужно исправить проблему валидации');
+        cy.get('#exitMessageButton > .exitIcon')
+    })   
+    it('Конвертер регистров', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon')
+    })  
+})
+3
+.toExponential
